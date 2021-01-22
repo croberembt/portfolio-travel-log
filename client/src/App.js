a1dd026ca62a78b0799245fe624f1b03733beaec
Calli Oberembt <croberembt@gmail.com>
2021-01-21 18:09:47 -0600

    discontinued making rating component for now, started working on a popup form that will allow the user to add an entry at the location the user double clicks on the map

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import './index.css';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl'; 
 import { listLogEntries } from './api'; 
-import { Rating } from 'material-ui-rating'; 
 
 
 const App = () => {
@@ -12,7 +11,7 @@ const App = () => {
     const [showPopup, setShowPopup] = useState({
 
     }); 
-
+    
     const [viewport, setViewport] = useState({
         width: '100vw',
         height: '100vh',
@@ -26,12 +25,17 @@ const App = () => {
         })(); 
     }, []); 
 
+    const showAddMarkerPopup = (event) => {
+        console.log(event); 
+    };
+
     return (
         <ReactMapGL
             {...viewport}
             mapStyle='mapbox://styles/calcifer2021/ckjx74nhx1gdc17ncj4gappdq'
             mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
             onViewportChange={setViewport}
+            onDblClick={showAddMarkerPopup}
             >
             {
                 logEntries.map(entry => (
@@ -49,15 +53,15 @@ const App = () => {
                                 <svg 
                                     className='map-marker' 
                                     alt='map marker' 
-                                    viewBox="0 0 24 24" 
-                                    width="20" 
-                                    height="20" 
-                                    stroke="#F1D11E " 
-                                    strokeWidth="3" fill="none" 
-                                    strokeLinecap="round" 
-                                    strokeLinejoin="round">
-                                    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                                    <circle cx="12" cy="10" r="3"></circle>
+                                    viewBox='0 0 24 24' 
+                                    width='20' 
+                                    height='20' 
+                                    stroke='#F1D11E' 
+                                    strokeWidth='3' fill='none' 
+                                    strokeLinecap='round' 
+                                    strokeLinejoin='round'>
+                                    <path d='M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z'></path>
+                                    <circle cx='12' cy='10' r='3'></circle>
                                 </svg>
                             </div>
                         </Marker>
@@ -83,10 +87,7 @@ const App = () => {
                                                     {entry.description}
                                                 </div>
                                                 <div>
-                                                    <Rating 
-                                                        value={entry.vacation_rating}
-                                                        max={10}
-                                                    />
+                                                    {entry.rating}
                                                 </div>
                                                 <div style={{margin: '1rem', padding: '1rem'}}>
                                                     <img alt={entry.title + 'image'} className='log-image' src={entry.image}></img>
