1e4530b7a422bf000c9555d38eb7f9bc859d2669
Calli Oberembt <croberembt@gmail.com>
2021-09-05 13:00:27 -0500

    attempting redeploy again changes were not getting properly updated

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,14 @@
 const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:1337' : 'https://oberembt-travel-log-server.vercel.app/'; 
 
+console.log(API_URL); 
+
 export async function listLogEntries() {
-  const response = await fetch(`${API_URL}/api/logs`);
+  const response = await fetch(`${API_URL}/api/logs`, {
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
+
   return response.json(); 
 }
 
